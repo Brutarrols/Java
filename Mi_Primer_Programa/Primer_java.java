e4dd002c88f77c64dd38a33925c8718887ef5754
Ariel Curinao <arielcrinao@hotmail.cl>
2020-11-06 11:41:35 -0300

    Mi primer Programa

---
+++ Mi_Primer_Programa/Primer_java.java
@@ -0,0 +1,9 @@
+public class Primer_java
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Mi nombre es Ariel");
+        System.out.println("Tengo 29 años");
+        System.out.println("Mi ciudad natal es Santiago, Chile");
+    }
+}
