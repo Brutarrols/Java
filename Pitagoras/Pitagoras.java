c621dcdfc1d38883c20e72ef35f44098b83f89b9
Ariel Curinao <arielcrinao@hotmail.cl>
2020-11-09 10:21:00 -0300

    Teorema de Pitagoras

---
+++ Pitagoras/Pitagoras.java
@@ -0,0 +1,10 @@
+public class Pitagoras {
+    public double calcularHipotnusa(int catetoA, int catetoB){
+        
+        double h;
+
+        h = Math.sqrt((catetoA*catetoA) + (catetoB *catetoB)); 
+
+        return h;
+    }
+}
---
+++ Pitagoras/PitagorasTest.java
@@ -0,0 +1,9 @@
+
+
+public class PitagorasTest {
+    public static void main (String[] args){
+        Pitagoras pitagoras = new Pitagoras();
+
+        System.out.println(pitagoras.calcularHipotnusa(3,5));
+    }
+}
