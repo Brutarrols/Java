f92fc2585eac02cc9f23f44f998e99e2b4d5ed29
Brutarrols <arielcrinao@gmail.com>
2020-11-10 09:34:53 -0300

    FizzBuzz

---
+++ FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,10 @@
+public class FizzBuzzTest{
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+
+        fb.fizzbuzz(18);
+        fb.fizzbuzz(25);
+        fb.fizzbuzz(30);
+        fb.fizzbuzz(2);
+    }
+}
